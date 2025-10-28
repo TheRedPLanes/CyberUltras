b05141e4a0c883274e2e2be8485f7298181336e9
Mikogano <26.noah.misek@ksd.org>
2025-10-28 13:13:35 -0700

    Made animcontrols for enemies

---
+++ UltraCyber/Assets/Scripts/EnemyAnimControls.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimControls : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = GetComponent<Rigidbody2D>().velocity.x; 
+        float moveY = GetComponent<Rigidbody2D>().velocity.y;
+        GetComponent<Animator>().SetFloat("x", moveX);
+        GetComponent<Animator>().SetFloat("y", moveY);
+        if (moveX < 0)
+        {
+            //we're moving to the left
+            //flip our sprite
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else if (moveX > 0) ;
+    }
+}
+
